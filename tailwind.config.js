7d7e708f5ff532164c8e338f2a663579cbdcc216
shuchengc <cuishucheng@gmail.com>
2024-11-13 11:44:08 +1100

    feat: set up scaffold

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all components in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
